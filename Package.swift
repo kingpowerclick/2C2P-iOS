fa626929b2586bc6db8ac8ce008a1a7d626cdc6e
Wipoo Shinsirikul <749739+mylifeasdog@users.noreply.github.com>
2023-06-02 18:16:22 +0700

    Update to 4.5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,5 +14,5 @@ let package = Package(
   targets: [
     .binaryTarget(
        name: "PGW",
-       url: "https://github.com/kingpowerclick/2C2P-iOS/releases/download/4.4.1/PGW.xcframework.zip",
-       checksum: "83672cdd521541c280140645571f04f31412694074226dc5580bf60f3c51e828") ])
+       url: "https://github.com/kingpowerclick/2C2P-iOS/releases/download/4.5.0/PGW.xcframework.zip",
+       checksum: "87ff6744416fd8f9e6a1e405d7a562a096c5e73c8810eb9bc808222e23ec52d1") ])
